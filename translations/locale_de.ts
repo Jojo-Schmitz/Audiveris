f4c77c9545cf5f15d6496414577aa46c49ee8613
Joachim Schmitz <jojo@schmitz-digital.de>
2013-07-14 17:28:26 +0200

    Adding German translation

---
+++ translations/locale_de.ts
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de">
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="../audiverisProgress.ui" line="14"/>
+        <location filename="../audiverisProgress.ui" line="23"/>
+        <source>Converting...</source>
+        <translation>Konvertiere...</translation>
+    </message>
+    <message>
+        <location filename="../audiverisProgress.ui" line="43"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;MS Shell Dlg 2&apos;; font-size:8.25pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:8pt;&quot;&gt;Please report any bug with the log above at &lt;/span&gt;&lt;a href=&quot;http://kenai.com/projects/audiveris&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;http://kenai.com/projects/audiveris&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;MS Shell Dlg 2&apos;; font-size:8.25pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:8pt;&quot;&gt;Bitte jeden Fehler zusammen mit obigem Log melden an &lt;/span&gt;&lt;a href=&quot;http://kenai.com/projects/audiveris&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;http://kenai.com/projects/audiveris&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../audiverisProgress.ui" line="75"/>
+        <source>Cancel</source>
+        <translation>Abbrechen</translation>
+    </message>
+</context>
+<context>
+    <name>audiveris</name>
+    <message>
+        <location filename="../audiveris.js" line="45"/>
+        <source>MuseScore: Load PDF File</source>
+        <translation>MuseScore: Lade PFD-Datei</translation>
+    </message>
+    <message>
+        <location filename="../audiveris.js" line="114"/>
+        <location filename="../audiveris.js" line="121"/>
+        <source>Close</source>
+        <translation>Schließen</translation>
+    </message>
+    <message>
+        <location filename="../audiveris.js" line="115"/>
+        <source>Conversion failed!</source>
+        <translation>Konvertierung fehlgeschlagen!</translation>
+    </message>
+    <message>
+        <location filename="../audiveris.js" line="116"/>
+        <location filename="../audiveris.js" line="132"/>
+        <source>Conversion failed</source>
+        <translation>Konvertierung fehlgeschlagen</translation>
+    </message>
+    <message>
+        <location filename="../audiveris.js" line="124"/>
+        <location filename="../audiveris.js" line="125"/>
+        <source>Conversion successful</source>
+        <translation>Konvertierung erfolgreich</translation>
+    </message>
+    <message>
+        <location filename="../audiveris.js" line="131"/>
+        <source>Conversion failed: No MusicXML file generated</source>
+        <translation>Konvertierung fehlgeschlagen: Kein MusicXML erzeugt</translation>
+    </message>
+</context>
+</TS>
